9c4c1e68c0dc224332c65b8ca2fb1b84c7007e5a
shanks <shanks@wisdomtmt.com>
2016-09-25 00:51:14 +0800

    create package script

---
+++ createPackage.sh
@@ -0,0 +1,17 @@
+dir=$1
+package=$2
+cd $dir
+mkdir $package
+cd $package
+touch Package.swift
+echo "import PackageDescription
+
+let package = Package(
+    name: \"${package}\",
+    dependencies: []
+)" >> Package.swift
+mkdir Sources
+cd Sources
+touch main.swift
+echo "import Glibc
+print(\"Hello, world!\")" >> main.swift
